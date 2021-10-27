3e6d674456a4bf73efe807f6f9097a848fec7682
Журавлев Михаил Сергеевич <MSZhuravlev.SBT@sberbank.ru>
2021-10-27 12:08:15 +0300

    Initial commit

---
+++ src/main/java/org/example/action/ScriptActionGroup.java
@@ -0,0 +1,12 @@
+package org.example.action;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import org.jetbrains.annotations.NotNull;
+
+public class ScriptActionGroup extends DefaultActionGroup {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+}
